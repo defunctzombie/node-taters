b04e582c4f0f865a0b57b9f52eeef410c4f33ad5
Roman Shtylman <shtylman@gmail.com>
2012-11-24 14:04:37 -0500

    fix syntax error

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = function(opt, mware) {
     var tot = undefined;
 
     mware = mware || function(req, res, next) {
-        res.header('Cache-Control'. 'public, max-age=31536000');
+        res.header('Cache-Control', 'public, max-age=31536000');
         res.header('Vary', 'Accept-Encoding');
         next();
     };
